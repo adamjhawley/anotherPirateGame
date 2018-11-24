ceabc70a60113786983399590ff86f77298cd6b4
HarryBurge <burgeharry@gmail.com>
2018-11-24 12:43:08 +0000

    Enemy shoot complete, only issue shoots works to well

--- core/src/uk/ac/york/sepr4/object/entity/Enemy.java
+++ core/src/uk/ac/york/sepr4/object/entity/Enemy.java
@@ -6,8 +6,11 @@ import lombok.Data;
 import uk.ac.york.sepr4.object.projectile.ProjectileType;
 import uk.ac.york.sepr4.screen.GameScreen;
 
+import javax.print.attribute.standard.MediaSize;
 import javax.swing.text.MutableAttributeSet;
 import java.util.List;
+
+import static java.lang.Double.NaN;
 
 @Data
 public class Enemy extends LivingEntity {
@@ -38,42 +41,53 @@ public class Enemy extends LivingEntity {
      */
     public void act(float deltaTime) {
         Player player = GameScreen.getInstance().getEntityManager().getOrCreatePlayer();
-//        float resAngle = resultantAngle(player);
-//        resAngle = resAngle % (float)(2*Math.PI);
-//        setAngle(resAngle);
-//
-//        float f = f(player);
-//
-//        if((1-f) - f > 0.2){
-//            setAccelerating(true);
-//            setBraking(false);
-//        } else if(((1-f) - f > -0.2)){
-//            setAccelerating(false);
-//            setBraking(false);
-//        } else {
-//                setBraking(false);
-//                setAccelerating(true);
-//        }
-//        if(getSpeed() < getMaxSpeed()/5){
-//            setAccelerating(true);
-//            setBraking(false);
-//        }
+        float resAngle = resultantAngle(player);
+        resAngle = convertToRealAngle(resAngle);
+        setAngle(resAngle);
 
-        //Gdx.app.log("fire", Float.toString(perfectShotAngle(player)));
-        Gdx.app.log("angle", Float.toString(player.getAngle()));
-        Gdx.app.log("anglewithmod", Float.toString((float)(player.getAngle() % 2*Math.PI)));
-        ////////Error with angle mod might be the reason for everything going wrong so fix
-        ////////B is also coming out as minus which shouldnt happen
+        float f = f(player);
+
+        if((1-f) - f > 0.2){
+            setAccelerating(true);
+            setBraking(false);
+        } else if(((1-f) - f > -0.2)){
+            setAccelerating(false);
+            setBraking(false);
+        } else {
+                setBraking(false);
+                setAccelerating(true);
+        }
+        if(getSpeed() < getMaxSpeed()/5){
+            setAccelerating(true);
+            setBraking(false);
+        }
+
+
+        fire(perfectShotAngle(player));
+        //Need to workout some logic on whether to shoot tip(use B if over a certain value to stop shooting otherwise you get a weird line of shots)
         super.act(deltaTime);
+    }
+
+    private float convertToRealAngle(float angle){
+        if(angle >= 0) {
+            while (angle >= 0) {
+                angle -= 2*Math.PI;
+            }
+            angle += 2*Math.PI;
+        } else {
+            while(angle < 0) {
+                angle += 2*Math.PI;
+            }
+        }
+        return angle;
     }
 
     private float perfectShotAngle(Player player){
         boolean right;
         double theta;
-        double thetaP = player.getAngle() % 2*Math.PI;
+        double thetaP = convertToRealAngle(player.getAngle());
         double thetaTP = getAngleTowardsLE(player);
-        Gdx.app.log("thetaP", thetaP+"");
-        Gdx.app.log("thetaTP", thetaTP+"");
+
         if(thetaP<=thetaTP && thetaTP<=Math.PI){
             theta = thetaP + (Math.PI - thetaTP);
             right = true;
@@ -96,80 +110,40 @@ public class Enemy extends LivingEntity {
             theta = 0;
             right = true;
         }
-        Gdx.app.log("thetaP", theta+"");
-        if(right == true){
-            Gdx.app.log("right", "");
-        } else {
-            Gdx.app.log("left", "");
-        }
 
         double b = timeForPerfectShotToHit(player, (float)theta);
-        Gdx.app.log("b", b+"");
         double time = getDistanceToPlayer(player)/(getSpeed()+getSelectedProjectileType().getBaseSpeed());
-        Gdx.app.log("time", time+"");
 
 
+        double SE = (getSpeed()+getSelectedProjectileType().getBaseSpeed());
+        double SP = player.getSpeed();
 
-//        double shotAngle = getAngleTowardsLE(player) + (Math.PI - Math.acos(1-(Math.pow(time, 2)/(2*Math.pow(b, 2)))) - thetaP);
-//        double shotAngle = (Math.PI - Math.acos(1-(Math.pow(time, 2)/(2*Math.pow(b, 2)))) - theta);
-        double shotAngle = Math.acos(Math.pow(time, 2) / (2*b*time));
-        Gdx.app.log("shotangle", shotAngle+"");
+        double PM = b*SP;
+        double EP = time*SE;
+        double ME = b*SE;
+
+        double shotAngle = Math.acos(((ME*ME) + (EP*EP) - (PM*PM)) / (2*ME*EP));
 
         if(right == true){
             shotAngle = thetaTP - shotAngle;
         } else {
             shotAngle = thetaTP + shotAngle;
         }
-        return (float)shotAngle;
+        if(((ME*ME) + (EP*EP) - (PM*PM)) / (2*ME*EP) > 1 || ((ME*ME) + (EP*EP) - (PM*PM)) / (2*ME*EP) < -1){
+            shotAngle = thetaTP;
+        }
+
+        return convertToRealAngle((float)shotAngle);
     }
 
     private float timeForPerfectShotToHit(Player player, float theta){
-//        double a = Math.PI - 2;
-//        Gdx.app.log("a", a+"");
         double time = getDistanceToPlayer(player)/(getSpeed()+getSelectedProjectileType().getBaseSpeed());
-//        double test = 2*Math.cos(theta);
-//        if(test < 0){
-//            test = -test;
-//        }
-//
-       double b = Math.pow(time, 2) / 2*Math.cos(theta);
-//        double b = Math.pow(time, 2) / test;
+
+        double b = time / (2*Math.cos(theta));
+        if(b < 0){
+            b = -b;
+        }
         return (float)b;
-
-
-
-
-
-//        double thetaP;
-//
-//        if(getAngleTowardsLE(player) > (player.getAngle() % 2*Math.PI)){
-//            thetaP = getAngleTowardsLE(player) - (player.getAngle()% 2*Math.PI);
-//            if(thetaP > Math.PI){
-//                thetaP = 2*Math.PI - thetaP;
-//            }
-//        } else {
-//            thetaP = (player.getAngle() % 2*Math.PI) - getAngleTowardsLE(player);
-//            if(thetaP > Math.PI){
-//                thetaP = 2*Math.PI - thetaP;
-//            }
-//        }
-//        Gdx.app.log("ntp", (Math.PI - getAngleTowardsLE(player))+"");
-//        Gdx.app.log("player", (player.getAngle()% 2*Math.PI)+"");
-//        Gdx.app.log("thetap", thetaP+"");
-//        double b = 2*Math.sin(thetaP)*time;
-//        Gdx.app.log("b", b+"");
-//        double c = Math.pow(time, 2);
-//        Gdx.app.log("c", c+"");
-//
-//        double BtimeMinus = (-b-Math.sqrt(Math.pow(b, 2) - 4*a*c))/(2*a);
-//        double BtimePlus = (-b+Math.sqrt(Math.pow(b, 2) - 4*a*c))/(2*a);
-//
-//        Gdx.app.log("minus", BtimeMinus+"");
-//        Gdx.app.log("plus", BtimePlus+"");
-//        double bPlus = (Math.pow(time, 2) + Math.sqrt(9*Math.pow(time, 4)))/(4*time);
-//        double bMinus = (Math.pow(time, 2) - Math.sqrt(9*Math.pow(time, 4)))/(4*time);
-//        return (float)bPlus;
-
     }
 
     private float getDistanceToPlayer(Player player) {
@@ -185,7 +159,7 @@ public class Enemy extends LivingEntity {
 
     private float resultantAngle(Player player){
         float f = f(player);
-        float sigma = getAngleTowardsLE(player) - (player.getAngle() % (float)(2*Math.PI));
+        float sigma = getAngleTowardsLE(player) - convertToRealAngle(player.getAngle());
 
         float tp;
         float rp;
